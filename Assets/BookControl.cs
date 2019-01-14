04254d827ede5fa4c29b1454da171d67c057f885
dragon4147 <dragonlyj@gmail.com>
2019-01-14 11:44:24 +0800

    restructuring sample book

---
+++ Assets/BookControl.cs
@@ -0,0 +1,26 @@
+﻿using Fungus;
+using UnityEngine;
+
+public class BookControl : MonoBehaviour
+{
+    private const string PAGELIST_PATH = "PageList";
+    private const string FRONT_PAGE_NAME = "StartFrontPage";
+    private GameObject pageListObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var trans = this.transform.Find(PAGELIST_PATH);
+        pageListObj = trans.gameObject;
+        pageListObj.SetActive(false);
+    }
+
+    public void StartShowBook()
+    {
+        pageListObj.SetActive(true);
+        
+        var bookFlowchart = this.GetComponent<Flowchart>();
+        bookFlowchart.SendFungusMessage(FRONT_PAGE_NAME);
+
+    }
+}
---
+++ Assets/Scripts/MainControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainControl : MonoBehaviour
+{
+    private const int LOGO_SECONDS = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowLogo());
+    }
+
+    private IEnumerator ShowLogo()
+    {
+        yield return new WaitForSeconds(LOGO_SECONDS);
+        //hide logo
+        var obj = GameObject.Find("LogoCanvas");
+        obj.SetActive(false);
+        
+        ShowBook("SampleBookFlowchart");
+    }
+
+    private static void ShowBook(string bookName)
+    {
+        var obj = GameObject.Find(bookName);
+        var ctl = obj.GetComponent<BookControl>();
+        ctl.StartShowBook();
+    }
+}
