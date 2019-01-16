955db01bc7605f147fe69100b8beb8eedbeac10e
dragon4147 <dragonlyj@gmail.com>
2019-01-16 11:42:25 +0800

    fix scene serialization problem

--- Assets/Scripts/BookControl.cs
+++ Assets/Scripts/BookControl.cs
@@ -1,26 +1,67 @@
-﻿using Fungus;
+﻿using System;
+using System.Collections;
+using Fungus;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+[RequireComponent(typeof(Flowchart))]
 public class BookControl : MonoBehaviour
 {
+    [SerializeField] public string bookName;
+    
     private const string PAGELIST_PATH = "PageList";
     private const string FRONT_PAGE_NAME = "StartFrontPage";
     private GameObject pageListObj;
+    private Flowchart bookFlowchart;
 
     // Start is called before the first frame update
     void Start()
     {
-        var trans = this.transform.Find(PAGELIST_PATH);
+        InitGameObjects();
+
+        //pageListObj.SetActive(false);
+        Debug.Log(bookName+" book control start");
+    }
+
+    private void InitGameObjects()
+    {
+        var trans = transform.Find(PAGELIST_PATH);
         pageListObj = trans.gameObject;
-        pageListObj.SetActive(false);
+        bookFlowchart = GetComponent<Flowchart>();
+        
+#if UNITY_EDITOR
+        CheckResource();        
+#endif
+    }
+
+    private void CheckResource()
+    {
+        Assert.IsTrue(pageListObj != null);
+        Assert.IsTrue(bookFlowchart != null);
+        Assert.IsFalse(string.IsNullOrEmpty(bookName));
     }
 
     public void StartShowBook()
     {
-        pageListObj.SetActive(true);
+        gameObject.SetActive(true);
         
-        var bookFlowchart = this.GetComponent<Flowchart>();
-        bookFlowchart.SendFungusMessage(FRONT_PAGE_NAME);
+        StartCoroutine(StartFrontPage());
+    }
 
+    private IEnumerator StartFrontPage()
+    {
+        yield return null;
+        pageListObj.SetActive(true);
+        yield return null;
+        bookFlowchart.SendFungusMessage(FRONT_PAGE_NAME);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+
+#if UNITY_EDITOR
+        Assert.IsFalse(string.IsNullOrEmpty(bookName),"Invalid book name");
+#endif
     }
 }
--- Assets/Scripts/MainControl.cs
+++ Assets/Scripts/MainControl.cs
@@ -1,34 +1,84 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class MainControl : MonoBehaviour
 {
+    public GameObject bookListGameObject;
+    
+    private Dictionary<string,BookControl> bookList = new Dictionary<string,BookControl>();
+    
     private const int LOGO_SECONDS = 5;
     private const string LOGOPATH = "LogoCanvas";
     private GameObject logo;
 
+    #region sample book demo
+    
+    private const string SAMPLE_BOOK_NAME = "My birthday";
+    
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        logo = this.transform.Find(LOGOPATH).gameObject;
-        logo.SetActive(true);
-
+        InitGameObjects();
+        SearchBooks();
+        HideBooks();
+        
         StartCoroutine(ShowLogo());
+    }
+
+    private void InitGameObjects()
+    {
+        logo = transform.Find(LOGOPATH).gameObject;
+        
+#if UNITY_EDITOR
+        Assert.raiseExceptions = true;
+        CheckResource();        
+#endif
+        
+    }
+
+    public void CheckResource()
+    {
+        Assert.IsTrue(bookListGameObject != null);
+    }
+
+    private void SearchBooks()
+    {
+        var lst = bookListGameObject.GetComponentsInChildren<BookControl>(true);
+        foreach (var ctl in lst)
+        {
+            bookList.Add(ctl.bookName,ctl);
+        }
+    }
+
+    private void HideBooks()
+    {
+        foreach (var ctl in bookList.Values)
+        {
+            ctl.Hide();
+        }
     }
 
     private IEnumerator ShowLogo()
     {
+        logo.SetActive(true);
         yield return new WaitForSeconds(LOGO_SECONDS);
         //hide logo
         logo.SetActive(false);
         
-        ShowBook("SampleBookFlowchart");
+        ShowBook(SAMPLE_BOOK_NAME);
     }
 
-    private static void ShowBook(string bookName)
+    private void ShowBook(string bookName)
     {
-        var obj = GameObject.Find(bookName);
-        var ctl = obj.GetComponent<BookControl>();
-        ctl.StartShowBook();
+        BookControl ctl;
+        if (bookList.TryGetValue(bookName, out ctl))
+        {
+            ctl.StartShowBook();
+        }
     }
 }
